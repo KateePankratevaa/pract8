ad1fcdc70b40dcb2520c8644ebb3df265f39b09e
KateePankratevaa <124819002+KateePankratevaa@users.noreply.github.com>
2023-10-24 14:05:50 +0500

    Add files via upload

---
+++ WpfApp8/Classes/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp8
+{
+    class User: INotifyPropertyChanged
+    {
+        public string UserName { get; set; }
+        public string Status { get; set;  }
+        public DateTime LastLogin { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string username;
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+                if (PropertyChanged!=null)
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Username"));
+            }
+        }
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private string status;
+
+        public string Staus
+        { 
+            get
+            {
+                return status;
+            }
+            set
+            {
+                status = value;
+                NotifyPropertyChanged("Status");
+            }
+        }
+
+    }
+}
---
+++ WpfApp8/Pages/UserPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp8.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для UserPage.xaml
+    /// </summary>
+    public partial class UserPage : Page
+    {
+        private User user { get; set; } = new User();
+        public UserPage()
+        {
+            InitializeComponent();
+            user.UserName = "Vasya";
+            user.Status = "Manager";
+            user.LastLogin = new DateTime(2007, 07, 07);
+            DataContext = user;
+        }
+
+        private void ChangePropertiesButton_Click(object sender, RoutedEventArgs e)
+        {
+            user.UserName = "Natashya";
+            user.Status = "Admin";
+        }
+
+        private void CheckProperties_Click(object sender, RoutedEventArgs e)
+        {
+            string pattern = "UserName: {0}; Status: {1}";
+            MessageBox.Show(string.Format(pattern, user.UserName, user.Status));
+        }
+
+    }
+}
